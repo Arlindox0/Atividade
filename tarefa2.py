d644dda00e99e297f1d3dda561af6a83ae75de57
Arlindox0 <133605657+Arlindox0@users.noreply.github.com>
2023-10-04 08:28:25 -0300

    Add files via upload

---
+++ tarefa2.py
@@ -0,0 +1,19 @@
+"""
+Exercícios
+
+1) Mude a cor da linha
+2) Mude o formato da tartaruga
+3) Mude o tamanho do quadrado
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.color('blue')
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
